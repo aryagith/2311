b86ee5941177da4ad028075a7382feaf4759e32e
arya <aaryagosavi17@gmail.com>
2024-02-16 17:40:35 -0500

    Implemented Comment and Review classes

--- src/Models/Comment.java
+++ src/Models/Comment.java
@@ -1,4 +1,90 @@
 package Models;
 
+import java.util.*;
+
 public class Comment {
+    private User user;
+    private String content;
+    private Review review;//comment will be under this review
+    private Comment comment;//OR under this comment
+    private int noOfUpvotes;
+    private int noOfDownvotes;
+    private List<Comment> replies;
+    private int TotalVotes=(noOfUpvotes-noOfDownvotes);//like on reddit
+   
+    public Comment(User user,Review review){
+       this.content="";
+       this.review=review;
+       this.noOfDownvotes=0;
+       this.noOfUpvotes=0;
+       this.user=user;
+       this.replies=new ArrayList<Comment>();
+    }
+
+    public Comment(User user,Comment comment){
+        this.content="";
+        this.comment=comment;
+        this.noOfDownvotes=0;
+        this.noOfUpvotes=0;
+        this.user=user;
+        this.replies=new ArrayList<Comment>();
+    }
+    
+    public Comment getComment() {
+        return comment;
+    }
+    public int getNoOfDownvotes() {
+        return noOfDownvotes;
+    }
+    public int getNoOfUpvotes() {
+     return noOfUpvotes;
+    }
+    public String getContent() {
+        return content;
+    }
+    public List<Comment> getReplies() {
+        return replies;
+    }
+    public Review getReview() {
+        return review;
+    }
+    public User getUser() {
+        return user;
+    }
+    public void setComment(Comment comment) {
+        this.comment = comment;
+    }
+    public void setContent(String content) {
+        this.content = content;
+    }
+    public void setNoOfDownvotes(int noOfDownvotes) {
+        this.noOfDownvotes = noOfDownvotes;
+    }
+    public void setNoOfUpvotes(int noOfUpvotes) {
+        this.noOfUpvotes = noOfUpvotes;
+    }
+    public void setReplies(List<Comment> replies) {
+        this.replies = replies;
+    }
+    public void setReview(Review review) {
+        this.review = review;
+    }
+    public void setUser(User user) {
+        this.user = user;
+    }
+    public void UpvoteComment() {
+        this.noOfUpvotes++;
+    }
+    public void DownvoteComment() {
+        this.noOfDownvotes++;
+    }
+    public int getTotalVotes() {
+        return TotalVotes;
+    }
+    public void setTotalVotes(int totalVotes) {
+        TotalVotes = totalVotes;
+    }
+    public void addComment(Comment comment) {
+        this.replies.add(comment);
+    }
 }
--- src/Models/Review.java
+++ src/Models/Review.java
@@ -1,4 +1,60 @@
 package Models;
 
+import java.util.*;
+
 public class Review {
+    private Rating rating;
+    private Movie movie;
+    private List<Comment> replies;
+    private String title;
+    private User user;
+
+    public Review(User user,Movie movie){
+        title="";
+        rating=new Rating();
+        replies=new ArrayList<Comment>();
+        this.user=user;
+        this.movie=movie;
+    }
+    public Review(User user,Movie movie,Rating rating,String title){
+        this.title=title;
+        this.movie=movie;
+        this.rating=rating;
+        this.user=user;
+        this.replies=new ArrayList<Comment>();
+    }
+    
+    public Movie getMovie() {
+        return movie;
+    }
+    public Rating getRating() {
+        return rating;
+    }
+    public String getTitle() {
+        return title;
+    }
+    public List<Comment> getReplies() {
+        return replies;
+    }
+    public User getUser() {
+        return user;
+    }
+    public void setMovie(Movie movie) {
+        this.movie = movie;
+    }
+    public void setRating(Rating rating) {
+        this.rating = rating;
+    }
+    public void setReplies(List<Comment> replies) {
+        this.replies = replies;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public void setUser(User user) {
+        this.user = user;
+    }
+    public void addComment(Comment comment){
+       this.replies.add(comment);
+    }
 }
