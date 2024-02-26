d9982f24d4fc6f64b618ff8a0213ac4264472c29
Krish Shah <88362417+shah-krish@users.noreply.github.com>
2024-02-26 15:04:59 -0500

    Add files via upload

---
+++ 2311-itr1/src/main/java/Models/Review.java
@@ -0,0 +1,12 @@
+package Models;
+
+public class Review {
+
+private String content;
+private Double rating;
+
+    public Review(String s, double i) {
+        this.content = s;
+        this.rating = i;
+    }
+}
